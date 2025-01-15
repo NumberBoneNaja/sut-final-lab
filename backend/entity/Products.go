b1f816d8c44f9b2c76f2649a243fcb5d9a824325
NumberBoneNaja <mailforgithub@gmail.com>
2025-01-15 11:36:55 +0700

    test  - close #2

--- backend/entity/Products.go
+++ backend/entity/Products.go
@@ -5,7 +5,8 @@ import "gorm.io/gorm"
 type Products struct {
 	gorm.Model
 	Name string
-	Price float64 `valid:"range(1|100)~Price must be between 1 and 1000"`
+	Price float64 `valid:"range(1|100)~Price must be between 1 and 1000 ,required~Price is required"`
 	SKU string  
+
 	
 }
