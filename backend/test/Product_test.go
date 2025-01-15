0b652cbf1aa0e0379735e6b98ca3b4382dbd307f
NumberBoneNaja <mailforgithub@gmail.com>
2025-01-15 11:33:02 +0700

    test not nagative1 - close #2

--- backend/test/Product_test.go
+++ backend/test/Product_test.go
@@ -24,11 +24,11 @@ func TestProducts(t *testing.T){
 	})
 
 
-	t.Run(`test success`,func(t *testing.T)  {
+	t.Run(`Price not success`,func(t *testing.T)  {
 		product:=entity.Products{
 			Name: "test",
 			Price:-1,
-			// SKU:"pdd",
+			SKU:"pdd",
 		}
 		ok,err := govalidator.ValidateStruct(product)
 		if !ok {
@@ -38,4 +38,34 @@ func TestProducts(t *testing.T){
 		g.Expect(err).NotTo(BeNil())
 		g.Expect(err.Error()).To(Equal("Price must be between 1 and 1000"))
 	})
+
+	t.Run(`Price not `,func(t *testing.T)  {
+		product:=entity.Products{
+			Name: "test",
+			Price:0,
+			SKU:"pdd",
+		}
+		ok,err := govalidator.ValidateStruct(product)
+		if !ok {
+			fmt.Println("err is",err)
+		}
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("Price is required"))
+	})
+    t.Run(`SKU not pattern`,func(t *testing.T)  {
+		product:=entity.Products{
+			Name: "test",
+			Price:0,
+			SKU:"pdd",
+		}
+		ok,err := govalidator.ValidateStruct(product)
+		if !ok {
+			fmt.Println("err is",err)
+		}
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("Price is required"))
+	})
+	
 }
